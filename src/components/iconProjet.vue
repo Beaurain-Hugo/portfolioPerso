d8bc509c8f04910a2c9a6f33c89f338cbff512cf
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2025-10-21 13:39:47 +0200

    maj icon projet

--- src/components/iconProjet.vue
+++ src/components/iconProjet.vue
@@ -1,16 +1,27 @@
 <script setup>
 import ImgS from '../components/ImgS.vue'
+import { supabase, user } from '@/supabase';
 defineProps({
+    id: String,
     illustration: String,
+    slug: String,
 })
 
 </script>
 
 <template>
-    <div>
-        <ImgS 
-        class="w-32 h-32 sm:w-64 sm:h-64"
-        bucket="prive-images"
-        :name="illustration"/>
+    <div class="relative group">
+        <button 
+            v-if="user" 
+            @click="$router.push({ name: 'Projets-Projet-edit-id', params: { id: id } })"
+            class="hidden text-sm group-hover:block z-10 absolute top-0 right-0 bg-black p-1 border-double border-2">
+            Editer offre
+        </button>
+        <a :href="'/projets/projet/' + slug">
+            <ImgS 
+            class="h-32"
+            bucket="prive-images"
+            :name="illustration"/>
+        </a>
     </div>
 </template>
