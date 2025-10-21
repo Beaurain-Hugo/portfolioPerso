ffc74de3cbcf1825a8b6944d87b69fb2e5fdc6a4
Beaurain-Hugo <hugo.beaurain@edu.univ-fcomte.fr>
2025-10-21 13:40:17 +0200

    maj page d'accueil

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -3,19 +3,24 @@ import listProjets from '@/components/listProjets.vue'
 import listProjetsDev from '@/components/listProjetsDev.vue'
 import listProjetsDesign from '@/components/listProjetsDesign.vue'
 import listProjetsTruc from '@/components/listProjetsTruc.vue'
-import {ref} from 'vue'
+import newListProjets from '@/components/newListProjets.vue'
+import { ref, computed } from 'vue'
 import { ChevronDownIcon } from '@heroicons/vue/20/solid';
 import { onMounted } from 'vue';
 import { supabase } from '@/supabase';
 
-const title = ref()
+// const title = ref()
+const categories = ref(["universitaire", "professionnel"])
 const allProjetsVisible = ref(true)
 const devProjetsVisible = ref(false)
 const designProjetsVisible = ref(false)
 const trucProjetsVisible = ref(false)
 
+const categorieSelected = (cat) => {
+  categories.value=cat
+}
+
 const selectProjets = () => {
-    
     devProjetsVisible.value = false
     designProjetsVisible.value = false
     trucProjetsVisible.value = false
@@ -47,7 +52,7 @@ const selectTruc = () => {
     }
 
 onMounted( () => {
-  selectProjets()
+  categorieSelected(['universitaire', 'professionnel'])
 })
 </script>
 
@@ -70,27 +75,28 @@ mounted() {
 
 
 <template>
-  <main class="bg-black">
-    <div class="ml-5 sm:ml-10 pt-10">
+  <main class="">
+    <div class="bg-black ml-5 sm:ml-10 pt-10">
       <div class="flex flex-col-reverse pt-5">
         <h1 class="font-unbounded text-3xl sm:text-6xl pt-5">Hugo Beaurain</h1>
-        <h2 class="font-poppins text-xl sm:text-3xl">Développeur</h2>
+        <h2 class="font-poppins text-xl sm:text-3xl">Développeur web</h2>
       </div>
       <hr class="bg-cyan-400 h-1 w-1/2 sm:w-1/4 border-t-0 my-8">
       <div class="md:flex">
         <div class="font-poppins text-sm sm:text-xl">
-          <p>Je m'appelle Hugo Beaurain et j'ai 21 ans. Je suis passionné par le <strong class="text-cyan-400">développement web</strong> et je passe beaucoup de temps à apprendre et à mettre en pratique mes compétences en <strong class="text-cyan-400">programmation</strong>.</p>
-          <p class="mt-8">Je m'intéresse aussi à la conception web et à la création de sites web. La satisfaction que je tire de la création de quelque chose de nouveau me fascine.</p>
-          <img class="hidden md:block md:m-auto" src="@/assets/double_fleche.svg" alt="">
+          <p>Je suis Hugo Beaurain, étudiant en deuxième année du <a class="underline" href="https://stgi.umlp.fr/formation/master-psm/"> master Technologie de l'Information « Produits et Services Multimédias »</a> à Montbéliard. Avant ça, je suis passé par le BUT MMI de Montbéliard dans la spécialité "Développement web et dispositifs interactifs" ; c'est ici que j'ai découvert le développement web et que j'ai décidé d'en faire mon métier !</p>
+          <p class="mt-5">Le développement web n'est pas ma seule passion, loin de là ! Je suis quelqu'un qui aime toucher à tout</p>
+
+          <img class="hidden md:block md:m-auto" src="@/assets/double_fleche.svg" alt="Double flèche qui pointe vers le bas"> 
         </div>
         <div class="flex md:flex-col mr-0 md:-mt-10">
-          <img class="block md:hidden m-auto" src="@/assets/double_fleche.svg" alt="">
+          <img class="block md:hidden m-auto" src="@/assets/double_fleche.svg" alt="Double flèche qui pointe vers le bas">
           <div class="hidden md:block z-0 w-36 h-36 sm:w-96 sm:h-96 bg-cyan-500 rounded-full blur-3xl opacity-60"></div>
           <img class="relative w-1/2 mr-0 md:w-full md:-mt-96" src="@/assets/photo_hugo.webp" alt="">
         </div>
       </div>
     </div>
-    <div class="bg-zinc-900 pt-5 sm:pt-10">
+    <!-- <div class="bg-zinc-900 pt-5 sm:pt-10">
       <ul class="font-unbounded text-sm text-slate-400 flex justify-evenly mx-5">
         <li class="relative"><button class="active" @click="selectProjets" href="">Projets récents</button></li>
         <li class="relative"><button @click="selectDev">Projets universitaires</button></li>
@@ -118,10 +124,24 @@ mounted() {
         </div>    
       </div>
       <img class="-mt-10 w-1/2 sm:w-1/4" src="@/assets/wave1.svg" alt="">
+    </div> -->
+    <div class="bg-zinc-900 pt-5 sm:pt-10">
+      <ul class="font-unbounded text-sm text-slate-400 flex justify-evenly mx-5 mb-5">
+        <li class="relative"><button class="active" @click="categorieSelected(['universitaire', 'professionnel'])" href="">Projets récents</button></li>
+        <li class="relative"><button @click="categorieSelected(['universitaire'])">Projets universitaires</button></li>
+        <li class="relative"><button @click="categorieSelected(['professionnel'])">Projets clients</button></li>
+      </ul>
+      <newListProjets :max=3 :categorie="categories" />
+      <div class="w-max my-5 mx-auto bg-sky-500 text-xs sm:text-2xl rounded-xl p-1">
+        <div class="bg-black h-full w-full rounded-lg px-8 py-1">
+          <RouterLink to="/projets" class="font-unbounded font-regular ">
+            <span>Voir plus</span>
+          </RouterLink>
+        </div>    
+      </div>
     </div>
-    <div class="bg-zinc-900 pb-10 relative">
-      <img class="w-1/3 sm:w-1/4" src="@/assets/wave2.svg" alt="">  
-      <div class="border-2 border-cyan-500 w-3/4 h-fit m-auto py-5 bg-zinc-700 absolute top-0 left-1/2 -translate-x-1/2">
+    <div class="bg-zinc-900 py-10 relative">
+      <div class="border-2 border-cyan-500 w-3/4 h-fit m-auto py-5 bg-zinc-700 abslute top-0 left-1/2">
         <div id="contact" class="bg-[url('src/assets/morphCircle.svg')] bg-contain bg-center bg-no-repeat">
           <div class="flex flex-col">
             <div>
@@ -138,7 +158,7 @@ mounted() {
               </ul>
             </div>
           </div>
-          <div class="flex justify-center font-poppins text-sm w-fit">
+          <div class="flex m-auto justify-center font-poppins text-sm w-fit">
             <img class="w-1/4" src="@/assets/arrow_turbine.svg" alt="">
             <a href="mailto:hugo.beaurain@yahoo.com">hugo.beaurain@yahoo.com</a>
           </div>
